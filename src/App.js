f63159a2cf190f4717437da00907798dba48b9d1
jupiter <jupiter.fierce.31@gmail.com>
2019-10-22 17:42:27 -0300

    first milestone

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import './App.css';
 import Cytoscape from 'cytoscape';
 import CytoscapeComponent from 'react-cytoscapejs';
 
-const elements = [
-  { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-  { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
-  { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
-];
 
 function App() {
   return (
