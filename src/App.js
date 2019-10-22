4e878d76f725d63b8cad1a50890a8c50cefc080c
jupiter <jupiter.fierce.31@gmail.com>
2019-10-22 17:31:40 -0300

    data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Cytoscape from 'cytoscape';
+import CytoscapeComponent from 'react-cytoscapejs';
+
+const elements = [
+  { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
+  { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
+  { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CytoscapeComponent
+        className="App-header"
+        pan={ { x: 100, y: 200 } } 
+        elements={CytoscapeComponent.normalizeElements({
+          nodes: [
+            { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
+            { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } }
+          ],
+          edges: [
+            {
+              data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' }
+            }
+          ]
+        })}
+       
+        stylesheet={[
+          {
+            selector: 'node',
+            style: {
+              width: 20,
+              height: 20,
+              shape: 'rectangle'
+            }
+          },
+          {
+            selector: 'edge',
+            style: {
+              width: 15
+            }
+          }
+        ]}
+      />
+
     </div>
   );
 }
