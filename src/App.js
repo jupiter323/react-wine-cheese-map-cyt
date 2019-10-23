0910ce6ed0e7e36e6c3c22a841999fbb643b35ba
jupiter <jupiter.fierce.31@gmail.com>
2019-10-23 16:27:04 -0300

    nodes add

--- src/App.js
+++ src/App.js
@@ -2,41 +2,18 @@ import React from 'react';
 import './App.css';
 import Cytoscape from 'cytoscape';
 import CytoscapeComponent from 'react-cytoscapejs';
-
+import data from './assets/data.json'
 
 function App() {
+  console.log(data)
   return (
     <>
       <CytoscapeComponent
         className="App-header"
-        pan={{ x: 700, y: 200 }}
-        elements={[
-          {
-            'data': { 'id': 'one', 'label': 'Modified Color' },
-            'position': { 'x': 75, 'y': 75 },
-            'classes': 'red'
-          },
-          {
-            'data': { 'id': 'two', 'label': 'Modified Shape' },
-            'position': { 'x': 75, 'y': 200 },
-            'classes': 'triangle'
-          },
-          {
-            'data': { 'id': 'three', 'label': 'Both Modified' },
-            'position': { 'x': 200, 'y': 75 },
-            'classes': 'red triangle'
-          },
-          {
-            'data': { 'id': 'four', 'label': 'Regular' },
-            'position': { 'x': 200, 'y': 200 }
-          },
-          { 'data': { 'source': 'one', 'target': 'two' }, 'classes': 'red',"selected" : false },
-          { 'data': { 'source': 'two', 'target': 'three' } },
-          { 'data': { 'source': 'three', 'target': 'four' }, 'classes': 'red' },
-          { 'data': { 'source': 'two', 'target': 'four' },"selected" : true },
-        ]}     
-        // layout={{ 'name': 'preset' }}
-        style={{ 'width': '100%', 'height': '400px' }}
+        // pan={{ x: 700, y: 200 }}
+        elements={data.elements.nodes}
+        layout={{ 'name': 'preset' }}
+        // style={{ 'width': '100%', 'height': '400px' }}
 
         stylesheet={[
           {
